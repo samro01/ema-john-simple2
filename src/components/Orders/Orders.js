37c5efbd991e5623e83278bf63c2642d6c1f782c
samro01 <srozario22@gmail.com>
2022-10-08 13:26:49 +0200

    remove from id added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
@@ -7,12 +8,20 @@ const Orders = () => {
     const { products, initialCart } = useLoaderData();
     const [cart, setCart] = useState(initialCart)
 
+    const handleRemoveItem = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
     return (
         <div className='shop-container'>
             <div className="orders-container">
                 {
                     cart.map(product => <ReviewItem key={product.id}
-                        product={product}></ReviewItem>)
+                        product={product}
+                        handleRemoveItem={handleRemoveItem}
+                    ></ReviewItem>)
                 }
             </div>
             <div className="cart-container">
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 
-const ReviewItem = ({ product }) => {
-    const { name, price, quantity, shipping, img } = product;
+const ReviewItem = ({ product, handleRemoveItem }) => {
+    const {id, name, price, quantity, shipping, img } = product;
     return (
         <div className='review-item'>
             <div>
@@ -21,7 +21,7 @@ const ReviewItem = ({ product }) => {
                         {quantity}</small></p>
                 </div>
                 <div className="delete-container">
-                    <button className='btn-delete'>
+                    <button onClick ={()=>handleRemoveItem(id)}className='btn-delete'>
                         <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon></button>
                 </div>
             </div>
